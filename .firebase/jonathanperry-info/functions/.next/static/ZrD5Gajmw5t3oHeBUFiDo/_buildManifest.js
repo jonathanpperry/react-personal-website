1e49c8ac00eb5168da3815f57cfe82e20df1ffd5
Jonathan Perry <jpper@umich.edu>
2023-04-13 22:52:49 -0500

    Remove ignored files

--- .firebase/jonathanperry-info/functions/.next/static/ZrD5Gajmw5t3oHeBUFiDo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-bcf0ba6d4f231caf.js","static/chunks/d7eeaac4-e65c72aa50f14bbe.js","static/chunks/pages/index-961a4ac67be9ed00.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
